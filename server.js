98b187cbde374d1ee8cad68363f83e9718f77fc5
dnyaneshwar-gitte <166032735+dnyaneshwar-gitte@users.noreply.github.com>
2025-03-08 14:41:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const routes = require("./routes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("School Management API is running...");
+});
+
 app.use("/", routes);
 app.use((req, res, next) => {
     console.log(`Incoming request: ${req.method} ${req.url}`);
