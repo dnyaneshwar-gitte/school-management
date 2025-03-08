5e5b01f184ce553d36e4bb81b706e6c2ee0234cf
dnyaneshwar-gitte <166032735+dnyaneshwar-gitte@users.noreply.github.com>
2025-03-08 14:40:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
